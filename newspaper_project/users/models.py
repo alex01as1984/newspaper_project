7eb28e7f1a5109acb9465e9342af0f4379f65d58
alex01as1984 <49124051+alex01as1984@users.noreply.github.com>
2019-04-05 20:57:50 +0500

    null

--- newspaper_project/users/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser     # new 1
-# Create your models here.
-
-class CustomUser(AbstractUser):
-    age = models.PositiveIntegerField(null=True, blank=True)
