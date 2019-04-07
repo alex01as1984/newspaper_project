ec73ce495ddf5117205562b2260a446228e38fb1
alex01as1984 <49124051+alex01as1984@users.noreply.github.com>
2019-04-07 22:39:09 +0500

    email_smtp

--- newspaper_project/newspaper_project/settings.py
+++ newspaper_project/newspaper_project/settings.py
@@ -134,5 +134,5 @@ LOGOUT_REDIRECT_URL = 'home'
 # connect bootstrap
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-# local email_password_reset
+# local email_password_reset 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
