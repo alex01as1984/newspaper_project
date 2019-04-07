613282e11c9a2c8d0785f544ebd822b6b2beb758
alex01as1984 <49124051+alex01as1984@users.noreply.github.com>
2019-04-07 21:24:50 +0500

    new base's links app pages in project

--- newspaper_project/pages/views.py
+++ newspaper_project/pages/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView   # new 2
 # Create your views here.
+
+class HomePageView(TemplateView):       # inheritance in TemplateView
+    template_name = 'home.html'
