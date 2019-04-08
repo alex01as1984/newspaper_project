029aa79f3742723e9e71dc62fdc6aae9d835381d
alex01as1984 <49124051+alex01as1984@users.noreply.github.com>
2019-04-08 22:40:01 +0500

    end article app

--- newspaper_project/articles/urls.py
+++ newspaper_project/articles/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path(',<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'),              # new edit
     path('<int:pk>/detail/', ArticleDetailView.as_view(), name='article_detail'),           # new detail
     path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),           # new delete
-    path('<int:pk>/create/', ArticleCreateView.as_view(), name='article_new'),      # new cteate
+    path('new/', ArticleCreateView.as_view(), name='article_new'),      # new cteate
     path('', ArticleListView.as_view(), name='article_list'),                               # new article
 ]
