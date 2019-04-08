eae95cdce82c6956b197fe584a8f617328936e2c
alex01as1984 <49124051+alex01as1984@users.noreply.github.com>
2019-04-08 21:35:49 +0500

    article template new

--- newspaper_project/articles/urls.py
+++ newspaper_project/articles/urls.py
@@ -4,11 +4,13 @@ from .views import (
     ArticleUpdateView,
     ArticleDetailView,
     ArticleDeleteView,
+    ArticleCreateView,
 )
 
 urlpatterns = [
     path(',<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'),              # new edit
     path('<int:pk>/detail/', ArticleDetailView.as_view(), name='article_detail'),           # new detail
     path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),           # new delete
+    path('<int:pk>/create/', ArticleCreateView.as_view(), name='article_new'),      # new cteate
     path('', ArticleListView.as_view(), name='article_list'),                               # new article
 ]
